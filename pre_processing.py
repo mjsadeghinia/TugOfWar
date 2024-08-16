a234e57dfe6a6697b0c13c47cf958f6a685ab44c
Javad Sadeghinia <mjsadeghinia@outlook.com>
2024-08-16 13:59:05 +0000

    refactored

--- pre_processing.py
+++ pre_processing.py
@@ -8,39 +8,51 @@ from activation_model import (
     compute_delayed_activations_single_compartment,
 )
 
+
+# %%
+def pre_process(
+    outdir, geo_params, segmentation_schema, num_time_step, delay_mode, delay
+):
+    geo = create_ellipsoid_geometry(
+        folder=outdir / "lv",
+        geo_params=geo_params,
+        segmentation_schema=segmentation_schema,
+    )
+    delayed_cfun = get_cfun_for_altered_compartment(segmentation_schema)
+    delayed_activations = compute_delayed_activations_single_compartment(
+        geo.cfun, delayed_cfun, num_time_step=num_time_step, std=delay, mode=delay_mode
+    )
+    fname = outdir / "activation.xdmf"
+    save_activation_as_dolfin_function(geo, delayed_activations, fname)
+
+
 # %%
 geo_params = {
     "r_short_endo": 3,
     "r_short_epi": 3.75,
     "r_long_endo": 4.25,
     "r_long_epi": 5,
-    "mesh_size": 2.5,
+    "mesh_size": 1,
 }
-
-outdir = Path("00_results/multi_core")
-
 segmentation_schema = {
-    "num_circ_segments": 7,
+    "num_circ_segments": 18,
     "num_long_segments": 6,
 }
-
 num_time_step = 500
+# %%
 delay = 0.05
 delay_mode = "delay"
-# %%
-geo = create_ellipsoid_geometry(
-    folder=outdir / "lv", geo_params=geo_params, segmentation_schema=segmentation_schema
-)
+outdir = Path("00_results/Level I/") / f"{delay_mode}_{delay}"
+pre_process(outdir, geo_params, segmentation_schema, num_time_step, delay_mode, delay)
 
-# %%
-# delayed_activations = compute_delayed_activations(
-#     geo.cfun, num_time_step=num_time_step, std=delay, mode=delay_mode
-# )
-delayed_cfun = get_cfun_for_altered_compartment(segmentation_schema)
-delayed_activations = compute_delayed_activations_single_compartment(
-    geo.cfun, delayed_cfun, num_time_step=num_time_step, std=delay, mode=delay_mode
-)
-fname = outdir / "activation.xdmf"
-save_activation_as_dolfin_function(geo, delayed_activations, fname)
+# # %%
+# delay = 0.05
+# delay_mode = "activation"
+# outdir = Path("00_results/Level I/") / f"{delay_mode}_{delay}"
+# pre_process(outdir, geo_params, segmentation_schema, num_time_step, delay_mode, delay)
 
-# %%
+# # %%
+# delay = 0.05
+# delay_mode = "diastole_time"
+# outdir = Path("00_results/Level I/") / f"{delay_mode}_{delay}"
+# pre_process(outdir, geo_params, segmentation_schema, num_time_step, delay_mode, delay)
--- processing.py
+++ processing.py
@@ -13,6 +13,56 @@ from heart_model import HeartModelPulse
 # %%
 # Dimensions
 # [ms] [cm] [ml] [kPa]
+
+
+def process(
+    outdir, num_time_step, t_end, circ_params, bc_params, atrium_pressure, comm=None
+):
+    if comm is None:
+        comm = dolfin.MPI.comm_world
+    fname = outdir / "lv/geo.h5"
+    geo = cardiac_geometries.geometry.Geometry.from_file(fname.as_posix(), comm=comm)
+    heart_model = HeartModelPulse(geo=geo, bc_params=bc_params, comm=comm)
+
+    circulation_model = CirculationModel(params=circ_params)
+    collector = DataCollector(outdir=outdir, problem=heart_model)
+    # saving initial values
+    v = heart_model.compute_volume(activation_value=0, pressure_value=0)
+    collector.collect(
+        time=0,
+        pressure=0,
+        volume=v,
+        activation=0.0,
+        flow=circulation_model.flow,
+        p_ao=circulation_model.aortic_pressure,
+    )
+
+    volume = heart_model.initial_loading(atrium_pressure=atrium_pressure)
+    collector.collect(
+        time=0,
+        pressure=atrium_pressure,
+        volume=volume,
+        activation=0.0,
+        flow=circulation_model.flow,
+        p_ao=circulation_model.aortic_pressure,
+    )
+
+    t_span = (0.0, 1.0)
+    t_eval = np.linspace(*t_span, num_time_step)
+    activation_fname = outdir / "activation.xdmf"
+
+    collector = circulation_solver(
+        heart_model=heart_model,
+        circulation_model=circulation_model,
+        activation_fname=activation_fname,
+        time=t_eval[:t_end] * 1000,
+        collector=collector,
+        start_time=2,
+        comm=comm,
+    )
+
+
+# %%
 comm = dolfin.MPI.comm_world
 circ_params = {
     "aortic_resistance": 5,
@@ -22,58 +72,24 @@ circ_params = {
     "diastolic_pressure": 10,
     "initial_pressure": 0.0,
 }
-
 bc_params = {"pericardium_spring": 0.0001}
-
 atrium_pressure = 1
-
 num_time_step = 500
 t_end = 350
-
-outdir = Path("00_results/multi_core")
-
-delay = 0.03
-delay_mode = "delay"
-
-fname = outdir / "lv/geo.h5"
-geo = cardiac_geometries.geometry.Geometry.from_file(fname.as_posix(), comm=comm)
-heart_model = HeartModelPulse(geo=geo, bc_params=bc_params, comm=comm)
-
-
-circulation_model = CirculationModel(params=circ_params)
-collector = DataCollector(outdir=outdir, problem=heart_model)
-# saving initial values
-v = heart_model.compute_volume(activation_value=0, pressure_value=0)
-collector.collect(
-    time=0,
-    pressure=0,
-    volume=v,
-    activation=0.0,
-    flow=circulation_model.flow,
-    p_ao=circulation_model.aortic_pressure,
-)
-
-volume = heart_model.initial_loading(atrium_pressure=atrium_pressure)
-collector.collect(
-    time=0,
-    pressure=atrium_pressure,
-    volume=volume,
-    activation=0.0,
-    flow=circulation_model.flow,
-    p_ao=circulation_model.aortic_pressure,
-)
-
-t_span = (0.0, 1.0)
-t_eval = np.linspace(*t_span, num_time_step)
-activation_fname = outdir / "activation.xdmf"
-
-collector = circulation_solver(
-    heart_model=heart_model,
-    circulation_model=circulation_model,
-    activation_fname=activation_fname,
-    time=t_eval[:t_end] * 1000,
-    collector=collector,
-    start_time=2,
-    comm=comm,
-)
 # %%
+delay = 0.05
+delay_mode = "delay"
+outdir = Path("00_results/Level I/") / f"{delay_mode}_{delay}"
+process(outdir, num_time_step, t_end, circ_params, bc_params, atrium_pressure)
+
+# %%
+delay = 0.05
+delay_mode = "activation"
+outdir = Path("00_results/Level I/") / f"{delay_mode}_{delay}"
+process(outdir, num_time_step, t_end, circ_params, bc_params, atrium_pressure)
+
+# %%
+delay = 0.05
+delay_mode = "diastole_time"
+outdir = Path("00_results/Level I/") / f"{delay_mode}_{delay}"
+process(outdir, num_time_step, t_end, circ_params, bc_params, atrium_pressure)
