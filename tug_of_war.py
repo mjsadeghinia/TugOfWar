c327ce19988b94a9e9aa8873d9a927564a6589c0
Javad Sadeghinia <mjsadeghinia@outlook.com>
2024-08-22 08:38:09 +0000

    modularized

--- tug_of_war.py
+++ tug_of_war.py
@@ -11,7 +11,155 @@ logger = get_logger()
 
 
 # %%
-def check_params_data(args):
+def parse_arguments(args=None):
+    """
+    Parse the command-line arguments.
+    """
+    parser = argparse.ArgumentParser()
+
+    # Geometry parameters
+    parser.add_argument(
+        "-m",
+        "--mesh_size",
+        default=2,
+        type=float,
+        help="The mesh size, approximate length of the edge for tetrahedrons [in cm]",
+    )
+    parser.add_argument(
+        "--r_short_endo",
+        default=3,
+        type=float,
+        help="The short radius of endocardium [in cm] for generating a simplified ellipsoid left ventricle model",
+    )
+    parser.add_argument(
+        "--r_short_epi",
+        default=3.75,
+        type=float,
+        help="The short radius of epicardium [in cm] for generating a simplified ellipsoid left ventricle model",
+    )
+    parser.add_argument(
+        "--r_long_endo",
+        default=4.25,
+        type=float,
+        help="The long radius of endocardium [in cm] for generating a simplified ellipsoid left ventricle model",
+    )
+    parser.add_argument(
+        "--r_long_epi",
+        default=5,
+        type=float,
+        help="The long radius of epicardium [in cm] for generating a simplified ellipsoid left ventricle model",
+    )
+
+    # Segmentation parameters
+    parser.add_argument(
+        "-c",
+        "--num_circ_segments",
+        default=18,
+        type=int,
+        help="The number of circumferential compartments per slice",
+    )
+    parser.add_argument(
+        "-r",
+        "--num_long_segments",
+        default=6,
+        type=int,
+        help="The number of slices (longitudinal)",
+    )
+
+    # Scenario and parameters
+    valid_scenarios = [
+        "single_compartment",
+        "homogenous_compartment",
+        "heterogenous_compartment",
+    ]
+    parser.add_argument(
+        "-s",
+        "--scenario",
+        choices=valid_scenarios,
+        default="homogenous_compartment",
+        type=str,
+        help="The scenario for the alteration of material properties. Must be one of: "
+             + ", ".join(valid_scenarios),
+    )
+
+    valid_params = [
+        "activation",
+        "passive_stiffness",
+        "active_stiffness",
+        "fiber_disarray",
+    ]
+    parser.add_argument(
+        "-p",
+        "--parameter",
+        choices=valid_params,
+        default="activation",
+        type=str,
+        help="The parameter of study to be modified. Must be one of: "
+             + ", ".join(valid_params),
+    )
+
+    # Additional arguments for 'activation'
+    valid_activation_mode = ["delay", "diastole_time", "systole_time", "decay"]
+    parser.add_argument(
+        "--activation_mode",
+        choices=valid_activation_mode,
+        type=str,
+        help="The mode of activation (required if 'activation' is chosen as parameter).",
+    )
+    parser.add_argument(
+        "--activation_variation",
+        type=float,
+        help="The amount of activation variation (required if 'activation' is chosen as parameter).",
+    )
+
+    # Additional settings
+    parser.add_argument(
+        "-t",
+        "--num_time_step",
+        default=500,
+        type=float,
+        help="The number of time steps between 0 and 1",
+    )
+    parser.add_argument(
+        "-o",
+        "--outdir",
+        default=str(Path.cwd()) + "/output",
+        type=str,
+        help="The output directory to save the files.",
+    )
+
+    return parser.parse_args(args)
+
+def create_geo_params(args):
+    """
+    Create a dictionary of geometry parameters from the parsed arguments.
+    """
+    return {
+        "r_short_endo": args.r_short_endo,
+        "r_short_epi": args.r_short_epi,
+        "r_long_endo": args.r_long_endo,
+        "r_long_epi": args.r_long_epi,
+        "mesh_size": args.mesh_size,
+    }
+
+def create_segmentation_schema(args):
+    """
+    Create a dictionary of segmentation schema from the parsed arguments.
+    """
+    return {
+        "num_circ_segments": args.num_circ_segments,
+        "num_long_segments": args.num_long_segments,
+    }
+
+def prepare_output_directory(args):
+    """
+    Prepare the output directory, ensuring it exists.
+    """
+    outdir_path = Path(args.outdir)
+    outdir_path.mkdir(exist_ok=True)
+    return outdir_path
+
+def check_parmas_activation(args):
     """
     Check if all the necessary input is provided otherwise assing a default vaule and issues warning to prevent any unwanted results.
     """
@@ -29,133 +177,16 @@ def check_params_data(args):
     return args
 
 
-def main() -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-m",
-        "--mesh_size",
-        default=2,
-        type=float,
-        help="The mesh size, approximate lenght of the edge for tetrahedrons [in cm]",
-    )
-    parser.add_argument(
-        "--r_short_endo",
-        default=3,
-        type=float,
-        help="The short radius of endo cardium [in cm] for generating a simplified ellipsoid left ventricle model",
-    )
-    parser.add_argument(
-        "--r_short_epi",
-        default=3.75,
-        type=float,
-        help="The short radius of epi cardium [in cm] for generating a simplified ellipsoid left ventricle model",
-    )
-    parser.add_argument(
-        "--r_long_endo",
-        default=4.25,
-        type=float,
-        help="The long radius of endo cardium [in cm] for generating a simplified ellipsoid left ventricle model",
-    )
-    parser.add_argument(
-        "--r_long_epi",
-        default=5,
-        type=float,
-        help="The long radius of epi cardium [in cm] for generating a simplified ellipsoid left ventricle model",
-    )
-    parser.add_argument(
-        "-c",
-        "--num_circ_segments",
-        default=18,
-        type=int,
-        help="The numer of circumferential compartments per slice",
-    )
-    parser.add_argument(
-        "-r",
-        "--num_long_segments",
-        default=6,
-        type=int,
-        help="The number of slices (longitudinal)",
-    )
+def main(args=None) -> int:
+    if args is None:
+        args = parse_arguments(args)
 
-    valid_scenarios = [
-        "single_compartment",
-        "homogenous_compartment",
-        "heterogenous_compartment",
-    ]
-    parser.add_argument(
-        "-s",
-        "--scenario",
-        choices=valid_scenarios,
-        default="homogenous_compartment",
-        type=str,
-        help="The scenario for the alteraion of material properties, Must be one of: "
-        + ", ".join(valid_scenarios),
-    )
+    geo_params = create_geo_params(args)
 
-    valid_params = [
-        "activation",
-        "passive_stiffness",
-        "active_stiffness",
-        "fiber_disarray",
-    ]
-    parser.add_argument(
-        "-p",
-        "--parameter",
-        choices=valid_params,
-        default="activation",
-        type=str,
-        help="The parameter of study to be the modified, Must be one of: "
-        + ", ".join(valid_params),
-    )
+    segmentation_schema = create_segmentation_schema(args)
 
-    # Add the additional arguments for 'activation'
-    valid_activation_mode = ["delay", "diastole_time", "systole_time", "decay"]
-    parser.add_argument(
-        "--activation_mode",
-        choices=valid_activation_mode,
-        type=str,
-        help="The mode of activation (required if 'activation' is chosen as parameter).",
-    )
-
-    parser.add_argument(
-        "--activation_variation",
-        type=float,
-        help="The amount of activation_variation (required if 'activation' is chosen as parameter).",
-    )
-
-    parser.add_argument(
-        "-t",
-        "--num_time_step",
-        default=500,
-        type=float,
-        help="The number of time step between 0 and 1",
-    )
-    parser.add_argument(
-        "-o",
-        "--outdir",
-        default=str(Path.cwd()) + "/output",
-        type=str,
-        help="The output directory to save the files. ",
-    )
-
-    args = parser.parse_args()
-
-    geo_params = {
-        "r_short_endo": args.r_short_endo,
-        "r_short_epi": args.r_short_epi,
-        "r_long_endo": args.r_long_endo,
-        "r_long_epi": args.r_long_epi,
-        "mesh_size": args.mesh_size,
-    }
-
-    segmentation_schema = {
-        "num_circ_segments": args.num_circ_segments,
-        "num_long_segments": args.num_long_segments,
-    }
-
-    outdir = Path(args.outdir)
-    outdir.mkdir(exist_ok=True)
-
+    outdir = prepare_output_directory(args)
+    
     ## Creating Geometry
     geo = geometry.create_ellipsoid_geometry(
         folder=outdir / "lv",
@@ -163,7 +194,7 @@ def main() -> int:
         segmentation_schema=segmentation_schema,
     )
 
-    args = check_params_data(args)
+    args = check_parmas_activation(args)
     parameter = args.parameter
     activation_mode = args.activation_mode
     activation_variation = args.activation_variation
