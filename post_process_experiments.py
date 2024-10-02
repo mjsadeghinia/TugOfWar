0502995d85157944afab6fb322dcf92c07db2d8b
Javad Sadeghinia <mjsadeghinia@outlook.com>
2024-10-02 08:40:23 +0000

    multiple experiemnt postprocessing with multicore

---
+++ post_process_experiments.py
@@ -0,0 +1,42 @@
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+
+s_values = [
+    # "single_compartment",
+    # "homogenous_compartment",
+    "heterogenous_compartment"
+]
+
+activation_modes = [
+    "delay", "diastole_time",
+    # "systole_time", "decay"
+]
+
+activation_variations = [0.01, 0.03, 0.05]
+
+num_circ_segments = [9,72]
+num_long_segments = [6]
+
+base_data_folder = "01_results_24_10_03"
+
+def post_process_experiment(s, activation_mode, activation_variation, num_circ_segments, num_long_segments):
+    data_folder = f"/home/shared/{base_data_folder}/{s}/{activation_mode}_{int(activation_variation * 1000)}"
+    outdir = f"{num_circ_segments}_{num_long_segments}"
+    cmd = [
+        "python3", "/home/shared/TugOfWar/post_processing.py",
+        "--data_folder", data_folder,
+        "--outdir", outdir,
+        "--num_circ_segments",f"{num_circ_segments}",
+        "--num_long_segments",f"{num_long_segments}",
+    ]
+    subprocess.run(cmd)
+
+with ThreadPoolExecutor(max_workers=8) as executor:
+    for s in s_values:
+        for activation_mode in activation_modes:
+            for activation_variation in activation_variations:
+                for c in num_circ_segments:
+                    for l in num_long_segments:
+                        executor.submit(post_process_experiment, s, activation_mode, activation_variation, c, l)
+
+
