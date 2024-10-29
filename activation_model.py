d6771b257a5f3fc6d168b8c710913d709d28cf14
Javad Sadeghinia <mjsadeghinia@outlook.com>
2024-10-29 12:05:50 +0000

    update create_infarct to take varname and fname

--- activation_model.py
+++ activation_model.py
@@ -718,16 +718,16 @@ class Infarct_expression(dolfin.UserExpression):
     def value_shape(self):
         return ()
     
-def create_infarct(outdir, geo, mi_center, mi_severity, iz_radius, bz_thickness, save_flag = True):
+def create_infarct(outdir, geo, mi_center, mi_severity, iz_radius, bz_thickness, save_flag = True, varname = "infarct", fname = "infarct"):
     V = dolfin.FunctionSpace(geo.mesh, "DG", 0)
     # center=(-1.47839,3.52203e-16,3.15815)
     infarct_expr = Infarct_expression(mi_center, mi_severity, iz_radius, bz_thickness)
     infarct = dolfin.interpolate(infarct_expr, V)
     if save_flag:
-        with dolfin.XDMFFile((outdir / "infarct.xdmf").as_posix()) as xdmf:
+        with dolfin.XDMFFile((outdir / f"{fname}.xdmf").as_posix()) as xdmf:
             xdmf.write_checkpoint(
                 infarct,
-                "infarct",
+                varname,
                 0.0,
                 dolfin.XDMFFile.Encoding.HDF5,
                 False,
