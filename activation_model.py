5c23643e50e47a888b9333ad191335cf88bba925
Javad Sadeghinia <mjsadeghinia@outlook.com>
2025-03-13 10:11:10 +0000

    added pure activation change in systole

--- activation_model.py
+++ activation_model.py
@@ -680,8 +680,8 @@ def cmpute_ep_activation(
             activation_params["t_sys"] = t_eval[ind]
             
             if randomized_flag:
-                activation_params["a_min"] *= (np.random.random() + 0.5)        # [0.5-1.5] 50% increase or decrease
-                activation_params["sigma_0"] *= (np.random.random()/2.5 + 0.8)  # [0.8-1.2] 20% increase or decrease
+                #activation_params["a_min"] *= (np.random.random() + 0.5)        # [0.5-1.5] 50% increase or decrease
+                #activation_params["sigma_0"] *= (np.random.random()/2.5 + 0.8)  # [0.8-1.2] 20% increase or decrease
                 activation_params["t_sys"]  *= (np.random.random() + 0.5)        # [0.5-1.5] 50% increase or decrease
                 # if activation_params["t_sys"]<0:
                 #     logger.warning('Systole time is negative, setting to 100 ms')
